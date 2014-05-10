51ff9afcf01504b44b01d043aa0b5f14b85c45c2
Tamara <tamarinda@gmail.com>
2014-05-10 12:05:17 +0200

    Initial Commit

---
+++ TamarindaKatas/TMRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMRAppDelegate.h
+//  TamarindaKatas
+//
+//  Created by mariachi on 10/05/14.
+//  Copyright (c) 2014 Tamara Bernad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TamarindaKatas/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TamarindaKatas
+//
+//  Created by mariachi on 10/05/14.
+//  Copyright (c) 2014 Tamara Bernad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMRAppDelegate class]));
+    }
+}
