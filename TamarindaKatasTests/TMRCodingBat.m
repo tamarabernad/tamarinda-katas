900890b88bbf5027c8ed4c083f28a571426c6380
Tamara <tamarinda@gmail.com>
2014-05-10 13:43:52 +0200

    no teen

--- TamarindaKatasTests/TMRCodingBat.m
+++ TamarindaKatasTests/TMRCodingBat.m
@@ -84,4 +84,83 @@
     
     XCTAssertEqual(0, [self loneSumWithA:3 AndB:3 AndC:3]);
 }
+
+/*
+ Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
+ 
+ luckySum(1, 2, 3) → 6
+ luckySum(1, 2, 13) → 3
+ luckySum(1, 13, 3) → 1
+ 
+ */
+- (NSInteger) luckySumWithA:( NSInteger )a AndB:(NSInteger)b AndC:(NSInteger)c{
+    
+    NSInteger sum = 0;
+    if(a != 13){
+        sum += a;
+        if(b != 13){
+            sum +=b;
+            if (c!=13) {
+                sum += c;
+            }
+        }
+    }
+    
+    return sum;
+}
+- (void)testLuckySum_1_2_3{
+    
+    XCTAssertEqual(6, [self luckySumWithA:1 AndB:2 AndC:3]);
+}
+
+- (void)testLuckySum_1_2_13{
+    
+    XCTAssertEqual(3, [self luckySumWithA:1 AndB:2 AndC:13]);
+}
+
+- (void)testLuckySum_1_13_3{
+    
+    XCTAssertEqual(1, [self luckySumWithA:1 AndB:13 AndC:3]);
+}
+
+/*
+ Given 3 int values, a b c, return their sum. However, if any of the values is a teen -- in the range 13..19 inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens. Write a separate helper "public int fixTeen(int n) {"that takes in an int value and returns that value fixed for the teen rule. In this way, you avoid repeating the teen code 3 times (i.e. "decomposition"). Define the helper below and at the same indent level as the main noTeenSum().
+ 
+ noTeenSum(1, 2, 3) → 6
+ noTeenSum(2, 13, 1) → 3
+ noTeenSum(2, 1, 14) → 3
+ 
+ */
+
+- (NSInteger) fixTeen:(NSInteger )n{
+    if(n >= 13 && n<=19){
+        return n == 15 || n == 16 ? n : 0;
+    }
+    return n;
+}
+
+- (NSInteger) noTeenSumWithA:( NSInteger )a AndB:(NSInteger)b AndC:(NSInteger)c{
+    
+    NSInteger sum = 0;
+    sum += [self fixTeen:a];
+    sum += [self fixTeen:b];
+    sum += [self fixTeen:c];
+    return sum;
+}
+- (void)testNoTeenSum_1_2_3{
+    
+    XCTAssertEqual(6, [self noTeenSumWithA:1 AndB:2 AndC:3]);
+}
+
+- (void)testNoTeenSum_2_13_1{
+    
+    XCTAssertEqual(3, [self noTeenSumWithA:2 AndB:13 AndC:1]);
+}
+
+- (void)testNoTeenSum_2_1_14{
+    
+    XCTAssertEqual(3, [self noTeenSumWithA:2 AndB:1 AndC:14]);
+}
+
+
 @end
